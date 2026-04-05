3c97686896eb1b1962ce19a30e17e81d9a5ed616
Vlad Kovach <vladkovach00@gmail.com>
2026-04-05 14:14:26 +0200

    fix : update cubes styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,12 +6,12 @@ const cubes = [
 	{ id: "cube-1", svg: "Html", svg2: "Css" },
 	{ id: "cube-2", svg: "Js", svg2: "Ts" },
 	{ id: "cube-3", svg: "React", svg2: "Next.js" },
-	{ id: "cube-4", svg: "Scss", svg2: "Redux" },
+	{ id: "cube-4", svg: "Scss", svg2: "Tailwind" },
 	{ id: "cube-5", svg: "Git", svg2: "GitHub" },
 	{ id: "cube-6", svg: "Angular", svg2: "Vue.js" },
-	{ id: "cube-7", svg: "Npm", svg2: "es6" },
-	{ id: "cube-8", svg: "Vite", svg2: "Webpack" },
-	{ id: "cube-9", svg: "Rest", svg2: "Jest" },
+	{ id: "cube-7", svg: "Python", svg2: "Django" },
+	{ id: "cube-8", svg: "Vite", svg2: "PostgreSQL" },
+	{ id: "cube-9", svg: "Rest", svg2: "Pytest" },
 ];
 const heroIdeas = ["imagine", "plan", "desire", "think"];
 const Hero = () => {
@@ -59,26 +59,24 @@ const Hero = () => {
 				</a>
 			</div>
 			<div
-				className={`grid grid-cols-3 gap-2 max-sm:w-[200px] max-sm:mt-8  content-center ${styles.scene}`}
+				className={`grid grid-cols-3 gap-4.5 max-sm:w-[200px] max-sm:mt-8  content-center ${styles.scene}`}
 			>
 				{cubes.map((cube) => (
 					<div key={cube.id} className={styles.cube}>
 						<div
-							className={`${styles.face} ${styles.face_front} dark:bg-white bg-grayDark  inline-flex items-center justify-center`}
+							className={`${styles.face} ${styles.face_front}  inline-flex items-center justify-center`}
 						>
 							<GetSvg svg={cube.svg} />
 						</div>
-						<div className={`${styles.face} ${styles.face_back} dark:bg-white bg-grayDark `}></div>
+						<div className={`${styles.face} ${styles.face_back} `}></div>
 						<div
-							className={`${styles.face} ${styles.face_right} inline-flex items-center justify-center dark:bg-white bg-grayDark `}
+							className={`${styles.face} ${styles.face_right} inline-flex items-center justify-center `}
 						>
 							<GetSvg svg={cube.svg2} />
 						</div>
-						<div className={`${styles.face} ${styles.face_left} dark:bg-white bg-grayDark `}></div>
-						<div className={`${styles.face} ${styles.face_top} dark:bg-white bg-grayDark `}></div>
-						<div
-							className={`${styles.face} ${styles.face_bottom} dark:bg-white bg-grayDark `}
-						></div>
+						<div className={`${styles.face} ${styles.face_left} `}></div>
+						<div className={`${styles.face} ${styles.face_top} `}></div>
+						<div className={`${styles.face} ${styles.face_bottom} `}></div>
 					</div>
 				))}
 			</div>
