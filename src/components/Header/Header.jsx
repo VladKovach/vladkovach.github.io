bbf17388696f24d258c8580c39166cd7dc37b646
Vlad Kovach <vladkovach00@gmail.com>
2026-04-09 16:03:16 +0200

    fix : header layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ export const Header = () => {
 
 	return (
 		<header
-			className={`${styles.header}  p-4 w-full bg-backgroundLight dark:bg-backgroundDark   duration-100 flex justify-between items-center`}
+			className={`${styles.header} p-4 pl-24 max-mobile:pl-4  w-full bg-backgroundLight dark:bg-backgroundDark   duration-100 flex justify-between items-center`}
 			style={{
 				animationName: theme === "dark" ? "scrollheaderDark" : "scrollheader",
 				animationFillMode: "both",
@@ -21,48 +21,50 @@ export const Header = () => {
 				animationRange: "50px 140px",
 			}}
 		>
-			<span className="">
-				<img src={myPhoto} width="50px" height="50px" alt="my photo" />
-			</span>
+			<div className="max-w-[1100px] w-full  mx-auto  flex justify-between items-center">
+				<span className="">
+					<img src={myPhoto} width="50px" height="50px" alt="my photo" />
+				</span>
 
-			<nav className="hidden mobile:flex space-x-4">
-				<NavLinks onLinkClick={() => setIsMenuOpen(false)} />
-			</nav>
+				<nav className="hidden mobile:flex space-x-4">
+					<NavLinks onLinkClick={() => setIsMenuOpen(false)} />
+				</nav>
 
-			<button
-				onClick={toggleMenu}
-				aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-				aria-expanded={isMenuOpen}
-				aria-controls="mobile-menu"
-				className={`${
-					styles.menuBtn
-				}  hidden border-4 border-backgroundDark dark:border-backgroundLight dark:text-backgroundLight before:bg-backgroundDark dark:before:bg-backgroundLight after:bg-backgroundDark dark:after:bg-backgroundLight  max-mobile:block  z-41 ${
-					isMenuOpen ? styles.menuCloseBtn : ""
-				}`}
-			></button>
-			<div className="flex items-center space-x-6">
-				<LanguageSwitcher />
-				<ThemeToggle />
+				<button
+					onClick={toggleMenu}
+					aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+					aria-expanded={isMenuOpen}
+					aria-controls="mobile-menu"
+					className={`${
+						styles.menuBtn
+					}  hidden border-4 border-backgroundDark dark:border-backgroundLight dark:text-backgroundLight before:bg-backgroundDark dark:before:bg-backgroundLight after:bg-backgroundDark dark:after:bg-backgroundLight  max-mobile:block  z-41 ${
+						isMenuOpen ? styles.menuCloseBtn : ""
+					}`}
+				></button>
+				<div className="flex items-center space-x-6">
+					<LanguageSwitcher />
+					<ThemeToggle />
+				</div>
+
+				{/* for menuMobile  */}
+				<div
+					className={`scroll-y-0 mobile:hidden fixed top-0  left-0 w-full  pointer-events-none bg-backgroundLight dark:bg-backgroundDark  transition-opacity duration-300  h-full ${
+						isMenuOpen ? "z-40 opacity-100" : "z-[-10] opacity-0 "
+					}`}
+				/>
+
+				{isMenuOpen && (
+					<>
+						<div
+							className={`scroll-y-0 mobile:hidden fixed top-30 left-0   flex flex-col h-1/2  w-full justify-around  items-center ${
+								isMenuOpen ? "z-40 opacity-100" : "z-[-10] opacity-0"
+							}`}
+						>
+							<NavLinks onLinkClick={() => setIsMenuOpen(false)} />
+						</div>
+					</>
+				)}
 			</div>
-
-			{/* for menuMobile  */}
-			<div
-				className={`scroll-y-0 mobile:hidden fixed top-0  left-0 w-full  pointer-events-none bg-backgroundLight dark:bg-backgroundDark  transition-opacity duration-300  h-full ${
-					isMenuOpen ? "z-40 opacity-100" : "z-[-10] opacity-0 "
-				}`}
-			/>
-
-			{isMenuOpen && (
-				<>
-					<div
-						className={`scroll-y-0 mobile:hidden fixed top-30 left-0   flex flex-col h-1/2  w-full justify-around  items-center ${
-							isMenuOpen ? "z-40 opacity-100" : "z-[-10] opacity-0"
-						}`}
-					>
-						<NavLinks onLinkClick={() => setIsMenuOpen(false)} />
-					</div>
-				</>
-			)}
 		</header>
 	);
 };
@@ -80,15 +82,15 @@ function LanguageSwitcher() {
 					key={lng}
 					onClick={() => changeLanguage(lng)}
 					className={`
-            px-3 py-1 rounded shadow 
-            flex items-center
-            transition duration-200   mobile:px-1.5  max-mobile:py-0.5 max-mobile:px-1
-            ${
+						px-3 py-1 rounded shadow 
+						flex items-center
+						transition duration-200   mobile:px-1.5  max-mobile:py-0.5 max-mobile:px-1
+						${
 							i18n.language === lng
 								? "bg-gradient-to-r from-[#f57f17] to-[#e9a441] text-white shadow-lg scale-105"
 								: "bg-transparent text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700"
 						}
-          `}
+          			`}
 				>
 					<GetSvg svg={lng} />
 					<span className="ml-1 font-bold uppercase max-mobile:font-medium ">{lng}</span>
